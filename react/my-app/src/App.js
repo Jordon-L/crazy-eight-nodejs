9306f40a65e4381bf4d58e177746ca2c2bbdfc0c
Jordon-L <jordonlee1412@gmail.com>
2022-10-01 13:44:55 -0700

    remove some comments

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,31 +1,13 @@
 import 'App.css';
-import {SocketContext} from 'context/socket'
 import Game from "components/game/game";
 import Home from "components/home/home";
 import HowToPlay from "components/home/howToPlay";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 
 // Material Kit 2 React themes
 import theme from 'assets/theme';
-
-
-let playerName;
-
-// function App() {
-//     return (
-//         <div className="App">
-//             <ThemeProvider theme={theme}>
-//                 <SocketContext.Provider value={socket}>
-//                     <Game></Game>
-//                 </SocketContext.Provider>
-//             </ThemeProvider>
-
-//         </div>
-//     );
-// }
 
 const App = () => {
   return (
